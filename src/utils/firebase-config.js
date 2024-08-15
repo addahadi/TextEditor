ef978e42477dab3306b2ec2140520f7c3c2f47b0
addahadi <addajs48@gmail.com>
2024-08-15 16:07:34 +0100

    Updated Firebase config to use environment variables

--- src/utils/firebase-config.js
+++ src/utils/firebase-config.js
@@ -1,19 +1,19 @@
-// Import the functions you need from the SDKs you need
-import { getAuth, GoogleAuthProvider  } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
+
 // Your Firebase configuration object
 const firebaseConfig = {
-  apiKey: "AIzaSyANAD1hI8e3SE9yBUICcy7JHurvQ3y-98Q",
-  authDomain: "texteditor-3eb26.firebaseapp.com",
-  projectId: "texteditor-3eb26",
-  storageBucket: "texteditor-3eb26.appspot.com",
-  messagingSenderId: "1078422004489",
-  appId: "1:1078422004489:web:686ae0696b93ca136929d8"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
-export const app = !firebase.apps.length  ?  firebase.initializeApp(firebaseConfig) : firebase.app();
+export const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
 export const provider = new GoogleAuthProvider();
-export const auth = getAuth(app)
-export let  database = firebase.firestore()
+export const auth = getAuth(app);
+export const database = firebase.firestore();
